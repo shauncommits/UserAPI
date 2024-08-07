7f6732b6ec7ff9cfab9c3c6809280db3a0059c21
Shaun Mbolompo <Shaun.Mbolompo@derivco.co.za>
2024-08-07 10:11:43 +0200

    Edit Program.cs

--- User/Program.cs
+++ User/Program.cs
@@ -1,14 +1,33 @@
+using Microsoft.EntityFrameworkCore;
 using User.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add Scoped IUserFactory
 builder.Services.AddScoped<IUserFactory, UserFactory>();
+
+// Add DbContext Service
+string connectionString = "Server=127.0.0.1,1433;Database=Users;User Id=sa;Password=Password_123;TrustServerCertificate=true";
+
+builder.Services.AddDbContext<UserDBContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Adding Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -20,4 +39,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.MapControllers();
+app.UseCors();
+app.UseRouting();
+app.UseAuthorization();
 app.Run();
