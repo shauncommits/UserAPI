dfacf2b86a26df66e8461648d9f6eabc4ce0d35f
Shaun Mbolompo <Shaun.Mbolompo@derivco.co.za>
2024-08-12 10:22:35 +0200

    Update DbSet<User> table name to correspond to the created DB on the sql docker container

--- User/Models/UserDBContext.cs
+++ User/Models/UserDBContext.cs
@@ -6,5 +6,10 @@ public class UserDBContext: DbContext
 {
     public UserDBContext(DbContextOptions<UserDBContext> options): base(options){}
     
-    public DbSet<User> User { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(e => e.Id);
+    }
+    
+    public DbSet<User> Users { get; set; }
 }
